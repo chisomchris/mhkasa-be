8ca5546a789e3b66ca84b4ebf99a99de4bf27d69
chisomchris <christianezeanyaeche@gmail.com>
2024-03-05 21:09:10 +0100

    test: testing user agent IP details

--- middlewares/userAgent.js
+++ middlewares/userAgent.js
@@ -1,6 +1,21 @@
+const IP = require("ip");
+
 const userAgent = async (req, res, next) => {
   const userAgent = req.headers["user-agent"];
-  const ip = req.ip;
+  const remoteAddress = req.socket.remoteAddress;
+  const localAddress = req.socket.localAddress;
+  const localPort = req.socket.localPort;
+  const remotePort = req.socket.remotePort;
+  const expressIp = req.ip;
+  const Ip = IP.address();
+  console.log({
+    remoteAddress,
+    localAddress,
+    localPort,
+    remotePort,
+    expressIp,
+    Ip,
+  });
   req.client = `${ip}-${userAgent}`;
   next();
 };
